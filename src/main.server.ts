149dc9ff3294e9caa55ec45547549043d1fa8616
David Rodriguez <15853933+limbo56@users.noreply.github.com>
2024-05-06 09:12:29 -0400

    Transfer localstorage polyfill to server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import 'localstorage-polyfill'
-
-global['localStorage'] = localStorage;
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
